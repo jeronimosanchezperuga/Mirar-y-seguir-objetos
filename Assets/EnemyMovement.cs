d26de9fc207117d875d744b7b332b6fad3f9cabb
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2022-06-06 17:55:55 -0300

    Some exercises

--- Assets/EnemyMovement.cs
+++ Assets/EnemyMovement.cs
@@ -7,24 +7,10 @@ public class EnemyMovement : MonoBehaviour
     public float speed;
     public Transform targetTR;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
-
-        //transform.LookAt();
-        //transform.Translate(transform.forward * speed * Time.deltaTime);
         var step = speed * Time.deltaTime;
-        FollowUsingMoveTowards(step);
-    }
-
-    void FollowUsingMoveTowards(float followSpeed)
-    {
-        transform.position  = Vector3.MoveTowards(transform.position,targetTR.position,followSpeed);
+        transform.position = Vector3.MoveTowards(transform.position, targetTR.position, step);
     }
 }
